853cbb5654015cc4e6e208a62b7d0efd3196e619
qimanchen <1033178199@qq.com>
2019-05-10 22:26:21 +0800

    add topo reference file

--- Topobuild.m
+++ Topobuild.m
@@ -7,13 +7,21 @@ starxdegree=inf*ones(numnode);
 for i=1:numnode
     temp=1:numnode;
     for k=i+1:numnode
+		# length(find(starxdegree(k,:)==1)) -- 寻找矩阵中第k行中等于1的元素的个数，满足degree个数的节点
         if length(find(starxdegree(k,:)==1))>=degree
+			# find函数返回的是寻找找的元素的index索引位置
+			# 清除temp中等于k的元素，pop出去
             temp(find(temp==k))=[];
         end
     end
-    temp(1:i)=[];
+    temp(1:i)=[];# 去除temp中第一个到第i个元素
+	# 寻找到第i行（第i个节点）中等于1的节点
     findnode=degree-length(find(starxdegree(i,:)==1));
+	# 重新排序temp
+	# python中可以使用random.shuffle(list)实现randperm的功能
+	# 可以通过random.sample(list,n) -- 从list中选取n个随机的元素
     temp=temp(randperm(length(temp)));
+	# 寻找到相应直连节点
     if length(temp)>=findnode
         temp=temp(1:findnode);
         for j=1:findnode
--- wss_op_simulation/reference_topology.py
+++ wss_op_simulation/reference_topology.py
@@ -4,9 +4,43 @@
 """
 参考拓扑生成文件
 """
+import random
 
 def topo(num_node, degree):
 	"""
 	拓扑生成程序
 	"""
+	# 生成初始拓扑矩阵
+	start_degree = [[None for i in range(num_node)]for j in range(num_node)]
+	
+	for i in range(num_node):
+		temp = list(range(num_node))
+		for k in range(i+1, num_node):
+			if start_degree[k].count(1) >= degree:
+				# 寻找到等于1的节点的位置
+				while k in temp:
+					temp.remove(k)
+		n = i
+		
+		while n > -1:
+			temp.pop(0)
+			n -= 1
+		find_node = degree - start_degree[i].count(1)
+		
+		random.shuffle(temp)
+		
+		if len(temp) >= find_node:
+			temp = [temp[i] for i in range(find_node)]
+			for j in range(find_node):
+				start_degree[i][temp[j]] = 1
+				start_degree[temp[j]][i] = 1
+				
+	return start_degree
+				
+		
+	
+	
+
+if __name__ == "__main__":
+	print(topo(5,2))
 	
